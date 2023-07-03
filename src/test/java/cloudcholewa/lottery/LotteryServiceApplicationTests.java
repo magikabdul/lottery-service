6b4b26d6b00ef5b19cc3716f90528898a2a0621c
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2023-07-04 00:48:35 +0200

    initial repository

---
+++ src/test/java/cloudcholewa/lottery/LotteryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cloudcholewa.lottery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotteryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
